3a059a3c7a8ee72312ad453228898bfc334ee9ca
Wei-Yuan Cheng <sknc1993@gmail.com>
2024-10-23 00:07:51 -0400

    jokes

---
+++ src/Joke.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "./Joke.css";
+
+/** A single joke, along with vote up/down buttons. */
+
+const Joke = ({ id, vote, votes, text }) => {
+  function upVote(e) { vote(id, +1); }
+  function downVote(e) { vote(id, - 1); }
+
+  return (
+    <div className="Joke">
+      <div className="Joke-votearea">
+        <button onClick={upVote}>
+          <i className="fas fa-thumbs-up" />
+        </button>
+
+        <button onClick={downVote}>
+          <i className="fas fa-thumbs-down" />
+        </button>
+
+        {votes}
+      </div>
+      <div className="Joke-text">{text}</div>
+    </div>
+  );
+}
+
+export default Joke;
